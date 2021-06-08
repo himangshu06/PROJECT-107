acf7ec4161ca2defd08847c99de51f3c88ac0850
himangshu06 <76275772+himangshu06@users.noreply.github.com>
2021-06-08 20:12:29 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,63 @@
+var synth = window.speechSynthesis;
+
+    speak_data = "Welcome to Hand Gestures ";
+
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+
+    synth.speak(utterThis)
+
+
+    Webcam.set({
+    width:350,
+    height:300,
+    imageformat:"png",
+    png_quality:90
+});
+
+camera = document.getElementById("camera");
+Webcam.attach("#camera");
+
+function snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML='<img id="capture_image" src="'+data_uri+'"/>';
+
+    });
+}
+
+
+console.log('ml5 version:',ml5.version);
+classifier=ml5.imageClassifier('https://storage.googleapis.com/tm-model/Jwm0xTtMu/model.json',modelLoaded);
+
+function modelLoaded(){
+    console.log("modelLoaded!");
+}
+
+function check(){
+    img = document.getElementById("capture_image");
+    classifier.classify(img,gotResult);
+}
+
+function gotResult(error,results){
+ if(error){
+     console.log(error);
+ }
+ else{
+     console.log(results);
+     document.getElementById("result_object_name").innerHTML=results[0].label;
+     document.getElementById("result_object_accuracy").innerHTML=results[0].confidence.toFixed(3);
+ }
+}
+
+prediction_1 = "";
+
+function speak(){
+     var synth = window.speechSynthesis;
+     prediction_1 = document.getElementById("result_object_name");
+     speak_data1 = "The Hand Gesture is"+prediction_1;
+     var utterThis = new SpeechSynthesisUtterance(speak_data1);
+     synth.speak(utterThis);
+}
+
+
+
+
